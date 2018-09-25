674ecb5d10d47d9bd9e270f1be4f9f48f69d11ea
Beth Ehleringer <elizabethehleringer@gmail.com>
2018-09-25 04:46:38 -0400

    changed questions, background

--- assets/javascript/quiz.js
+++ assets/javascript/quiz.js
@@ -7,36 +7,37 @@ $( document ).ready(function() {
 //Array of objects that include questions, answers and images. The answers will be an array within this array.
 const questionArray = [
   {
-      question: "What color is the clear daytime sky?",
-      answers: { a: 'red', b: 'yellow', c: 'blue', d: 'green' },
+      question: "The Volusia County Fair is near what city?",
+      answers: { a: 'Daytona', b: 'Deltona', c: 'DeLand', d: 'Sanford' },
       correctAnswer: "c",
       image: "assets/images/placeholder.jpg"
   },
   {
-      question: "On which planet do we live?",
-      answers: { a: 'Earth', b: 'Mars', c: 'Jupiter', d: 'Saturn' },
-      correctAnswer: "a",
-      image: "assets/images/placeholder.jpg"
-  },
-
-  {
-      question: "What does Dominos deliver?",
-      answers: { a: 'doughnuts', b: 'pizza', c: 'eggplant', d: 'paper' },
-      correctAnswer: "b",
-      image: "assets/images/placeholder.jpg"
-  },
-
-  {
-      question: "What state holds the Minnesota State Fair?",
-      answers: { a: 'Florida', b: 'California', c: 'Iowa', d: 'Minnesota' },
+      question: "What would see in the small animal pavilion?",
+      answers: { a: 'guinea pigs', b: 'rabbits', c: 'roosters', d: 'all of the above' },
       correctAnswer: "d",
-      image: "assets/images/placeholder.jpg"
+      image: "assets/images/poultryRabbits.jpg"
   },
 
   {
-      question: "What flavor is hot chocolate?",
-      answers: { a: 'pineapple', b: 'vanilla', c: 'garlic', d: 'none of the above' },
-      image: "assets/images/placeholder.jpg"
+      question: "Which is not an event at VCF?",
+      answers: { a: 'Goat Costume Show', b: 'Steer Dancing', c: 'Poultry Fashion Show', d: 'Goat Meat Show' },
+      correctAnswer: "b",
+      image: "assets/images/cow.gif"
+  },
+
+  {
+      question: "What does the fair smell like?",
+      answers: { a: 'fried food', b: 'hay', c: 'manure', d: 'all of the above' },
+      correctAnswer: "d",
+      image: "assets/images/ferrisOninon.jpg"
+  },
+
+  {
+      question: "What is in the VCF parade?",
+      answers: { a: 'antique tractors ', b: 'goats', c: 'steers', d: 'pigs' },
+      correctAnswer: "a",
+      image: "assets/images/tractor.jpg"
   }
 ]
 
@@ -66,7 +67,7 @@ console.log("abcde" + userAnswers)
 // This function will replace display whatever image it's given
 // in the 'src' attribute of the img tag.
 function displayQA() {
-  $("#image-holder").html("<h1>" + questionArray[count].question + "</h1>" + "<h2>" + questionArray[count].answers.a + "</h2>" +
+  $("#image-holder").html("<h1>" + questionArray[count].question + "</h1>" + 
   "<form>" + "<input type='radio' name='answer' id='current-answer-a'  value='a'>" + questionArray[count].answers.a + "<br>" + 
   "<input type='radio' name='answer' id='current-answer-b'  value='b'>" + questionArray[count].answers.b + "<br>" + 
   "<input type='radio' name='answer' id='current-answer-c'  value='c'>" + questionArray[count].answers.c + "<br>" + 
